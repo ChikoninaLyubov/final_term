6add24aaf328ed583af3082006415f116056c928
lyubov <lyubov.chikonina@mail.ru>
2022-12-03 15:35:08 +0300

    program task

---
+++ Task_final/Program.cs
@@ -0,0 +1,21 @@
+﻿//Написать программу,которая из имеющегося массива строк формирует массив из строк,длина которых меньше либо равна 3 символа.Первоначальный массив можно ввести с клавиатуры,либо задать на старте выполнения алгоритма.
+//При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+Console.Clear();
+string[] in_mas = { "DayOfWeek", "devor", "del", "mar", "sam", "prokm", "end" };
+
+string[] SearchForThrdCharString(string[] in_mas)
+{
+    string[] out_mas = new string[1];
+    int j = 0;
+    for (int i = 0; i < in_mas.Length; i++)
+    {
+        if (in_mas[i].Length <= 3)
+        {
+            Array.Resize(ref out_mas, 1 + j);
+            out_mas[j] = in_mas[i];
+            j++;
+        }
+    }
+    return out_mas;
+}
+System.Console.WriteLine(String.Join("|", SearchForThrdCharString(in_mas)));
